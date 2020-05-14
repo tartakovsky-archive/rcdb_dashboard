ac31481850079c57e7e7865b3e308766e0b5e0b6
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-05-14 21:00:18 +0300

    Update settings.py

--- rcdb_execution/settings.py
+++ rcdb_execution/settings.py
@@ -96,7 +96,7 @@ if 'TEST_ENV' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
