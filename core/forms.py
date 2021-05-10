7408667871bef88760f28ace78c489a520874290
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2021-05-10 21:23:29 +0300

    rebate. exchange credentials select fixed

--- core/forms.py
+++ core/forms.py
@@ -6,8 +6,13 @@ TIMEFRAMES = [('1H', '1H'), ('1D', '1D'), ('1W', '1W'), ('1M', '1M')]
 DATETIME_LOCAL_WIDGET = forms.DateTimeInput(attrs={'type': 'datetime-local'})
 
 
+class ExchangeCredentialsChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj: ExchangeCredentials):
+        return f'{obj.name} | {obj.account_type_label} | {obj.label}'
+
+
 class RebatesForm(forms.Form):
-    exchange_credentials = forms.ModelChoiceField(queryset=ExchangeCredentials.objects.all())
+    exchange_credentials = ExchangeCredentialsChoiceField(queryset=ExchangeCredentials.objects.all())
     timeframe = forms.ChoiceField(choices=TIMEFRAMES, initial=TIMEFRAMES[0])
     start = forms.DateTimeField(widget=DATETIME_LOCAL_WIDGET, required=False)
     end = forms.DateTimeField(widget=DATETIME_LOCAL_WIDGET, required=False)
