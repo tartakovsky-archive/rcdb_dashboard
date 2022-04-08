7319794c24f6401258d741189139d32059a1f5b5
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2022-04-08 14:52:16 +0300

    added more task consumers

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,6 @@ while test $# -gt 0; do
       echo "Starting migration"
       docker-compose run web bash -c "./manage.py migrate"
       docker-compose down
-      docker-compose run web bash -c "./manage.py tradestohdf"
-      docker-compose down
       break
       ;;
     esac
